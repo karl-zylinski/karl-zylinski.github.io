672bca5800be13e31c97cd3e6d175371489d218d
Karl Zylinski <karl@zylinski.se>
2025-01-28 02:02:30 +0100

    odin-raylib-web sample bugfix

--- static/odin-raylib-web/index.js
+++ static/odin-raylib-web/index.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Karl\AppData\Local\Temp\tmp7po5q594.js
+// include: C:\Users\Karl\AppData\Local\Temp\tmpa12mpz_q.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -216,21 +216,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: C:\Users\Karl\AppData\Local\Temp\tmp7po5q594.js
-// include: C:\Users\Karl\AppData\Local\Temp\tmp9vrj97sx.js
+// end include: C:\Users\Karl\AppData\Local\Temp\tmpa12mpz_q.js
+// include: C:\Users\Karl\AppData\Local\Temp\tmphp54n3hk.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Karl\AppData\Local\Temp\tmp9vrj97sx.js
-// include: C:\Users\Karl\AppData\Local\Temp\tmp_jnvo9oz.js
+  // end include: C:\Users\Karl\AppData\Local\Temp\tmphp54n3hk.js
+// include: C:\Users\Karl\AppData\Local\Temp\tmpsvcndsav.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Karl\AppData\Local\Temp\tmp_jnvo9oz.js
+  // end include: C:\Users\Karl\AppData\Local\Temp\tmpsvcndsav.js
 
 
 // Sometimes an existing Module object exists with properties
